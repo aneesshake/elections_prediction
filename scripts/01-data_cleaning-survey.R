533e3f3bb17630bda93f0a4664e5e444f4d90910
jaffaromain <jaffa.romain@mail.utoronto.ca>
2020-10-29 23:32:23 -0400

    added to model + individual survey

--- scripts/01-data_cleaning-survey.R
+++ scripts/01-data_cleaning-survey.R
@@ -18,7 +18,7 @@ library(ggthemes)
 library(mapproj)
 
 us <- map_data("state")
-us<- us %>% mutate(state = case_when(state == "district of columbia"~"washington"))
+us <- us %>% mutate(region = case_when(region == "district of columbia"~"washington", TRUE ~ region))
 party_colors <- c("#FF4040","#0000FF")
 # Read in the raw data (You might need to change this if you use a different dataset)
 survey_data<- read_dta("inputs/raw_data/Nationscape/ns20200625.dta")
@@ -43,7 +43,7 @@ survey_data_reduced <-
          education,
          state,
          congress_district,
-         age)
+         age, )
 
 # Keep observations of those who intend to vote in the upcoming 2020 election
 survey_data_reduced <-survey_data_reduced %>% filter(vote_intention == "Yes, I will vote")
@@ -55,16 +55,38 @@ survey_data_reduced <-survey_data_reduced %>% filter(vote_2020 != "I am not sure
 # Keep vote results of either Democrat vs. Republican nominee
 survey_data_reduced <-survey_data_reduced %>% filter(vote_2020 == "Donald Trump"| vote_2020 == "Joe Biden")
 
-# data with only democratic and republican nominees from 2016
-survey_2016 <- survey_data_reduced <-survey_data_reduced %>% filter(vote_2016 == "Donald Trump"| vote_2016 == "Hillary Clinton")
-  
-# Age distribution - 2016 elections
-survey_data_reduced %>% ggplot(aes(x = age, fill  = vote_2020), color = "black") + geom_histogram(alpha = 0.7, color = "black", bins = 25) + 
-  scale_fill_manual(values = c("#FF4040", "#0000FF")) + labs(title = "Age Distribution" , subtitle =  "Comparing age distribution of those who intend to vote for Trump and Biden.", x = "Age", y= "Number of votes", fill = "Nominee") + facet_wrap(vars(gender))
+# matching state names to that of the post-stratification data
+survey_data_reduced <- survey_data_reduced %>% mutate(state = case_when(state == "AL" ~ "alabama", state == "AK" ~ "alaska", state == "AZ" ~ "arizona", state == "AR" ~ "arkansas", state == "CA" ~ "california",
+                                                                        state == "CO" ~ "colorado", state == "CT" ~ "connecticut", state == "DE" ~ "delaware", state == "FL"~ "florida", state == "GA" ~ "georgia", 
+                                                                        state == "HI" ~ "hawaii", state == "ID" ~ "idaho", state == "IL" ~ "illinois", state == "IN" ~ "indiana", state == "IA" ~ "iowa",state == "KS" ~ "kansas", 
+                                                                        state == "KY" ~ "kentucky", state == "LA" ~ "louisiana", state == "ME"~ "maine", state == "MD"~"maryland", state == "MA" ~ "massachusetts", state == "MI" ~ "michigan", 
+                                                                        state == "MN" ~ "minnesota", state == "MS" ~ "mississippi", state == "MO" ~ "missouri",
+                                                                        state == "MT" ~ "montana", state == "NE" ~ "nebraska", state == "NV" ~ "nevada", state == "NH"~ "new hampshire", state == "NJ"~"new jersey",
+                                                                        state == "NM" ~ "new mexico", state == "NY" ~ "new york", state == "NC" ~ "north carolina", state == "ND" ~ "north dakota", state == "OH" ~ "ohio",
+                                                                        state == "OK" ~ "oklahoma", state == "OR" ~ "oregon", state == "PA" ~ "pennsylvania", state == "RI"~ "rhode island", state == "SC"~"south carolina",
+                                                                        state == "SD" ~ "south dakota", state == "TN" ~ "tennessee", state == "TX" ~ "texas", state == "UT" ~ "utah", state == "VT" ~ "vermont",
+                                                                        state == "VA" ~ "virginia", state == "WA" ~ "washington", state == "WV" ~ "west virginia", state == "WI"~ "wisconsin", state == "WY"~"wyoming"))
+# matching hispanic variable to post stratification data set
+survey_data_reduced <- survey_data_reduced %>% mutate(hispan = ifelse(hispanic == "Not Hispanic", "not hispanic", ifelse(hispanic == "Mexican", "mexican", 
+                                              ifelse(hispanic == "Cuban", "cuban", ifelse(hispanic == "Puerto Rican", "puerto rican", "other")))))
+# matching race to post-stratification data set
+survey_data_reduced <- survey_data_reduced %>% mutate(race = ifelse(race_ethnicity == "White", "white", ifelse(race_ethnicity == "Black, or African American", "black", 
+    ifelse(race_ethnicity == "American Indian or Alaska Native", "american indian or alaska native",ifelse(race_ethnicity == "Asian (Asian Indian)", "asian", 
+    ifelse(race_ethnicity == "Asian (Chinese)", "asian",  ifelse(race_ethnicity == "Asian (Filipino)", "asian",  
+    ifelse(race_ethnicity == "Asian (Japanese)", "asian",  ifelse(race_ethnicity == "Asian (Korean)", "asian",  ifelse(race_ethnicity == "Asian (Vietnamese)", "asian", 
+    ifelse(race_ethnicity == "Asian (Other)", "asian",  ifelse(race_ethnicity == "Pacific Islander (Native Hawaiian)" , "asian", ifelse(race_ethnicity == "Pacific Islander (Guamanian)", "asian", 
+    ifelse(race_ethnicity == "Pacific Islander (Samoan)", "asian", ifelse(race_ethnicity == "Pacific Islander (Other)", "asian", "other" )))))))))))))))
+survey_data_reduced<- survey_data_reduced %>% mutate(age = case_when(age <= 24 ~ "18-24", age <= 44 ~ "25-44",
+    age <= 64 ~ "45-64",  age >= 65 ~ "65+"))
 
-# voters who voted for Trump in 2016 elections
-# 1948 voters
-survey_data_reduced %>% filter(vote_2016 == "Donald Trump")
+survey_data_reduced <- survey_data_reduced %>% mutate( sex = case_when(gender == "Female" ~ "female", gender == "Male"~ "male"))
+
+
+### GRAPHING
+s <- survey_data_reduced %>% select(vote_2016, race) %>% filter(vote_2016 != "Did not vote, but was eligible", vote_2016 != "Was not eligible to vote", vote_2016 != "Don't recall") %>% group_by(race, vote_2016) %>% summarise(n = n()) %>% mutate(freq = n/sum(n))
+## 2016 vote by race
+s %>% ggplot(aes(x = vote_2016, fill = race, group = race)) + geom_bar(aes(y = freq), stat = "identity", position = "dodge", alpha = 2/3) + labs(x = "Race", title = " Percentage of 2016 Presidential Election Votes by Race")
+
 # Voter intention by Income
 survey_data_reduced %>% drop_na(household_income) %>% ggplot(aes(x = household_income, fill  = vote_2020), color = "black") + geom_bar(alpha = 0.7, color = "black") + 
   scale_fill_manual(values = c("#FF4040", "#0000FF")) + labs(title = "Income Vote Distribution" , subtitle =  " A look into how eligible voters intend to vote based on income.", x = "Income", y= "Number of votes", fill = "Nominee") +   theme(axis.text.x = element_text(angle = 35, hjust = 1, size = 5))
@@ -72,35 +94,12 @@ survey_data_reduced %>% drop_na(household_income) %>% ggplot(aes(x = household_i
 # Voter intention by employment status
 survey_data_reduced %>% drop_na(employment) %>% ggplot(aes(x = employment, fill  = vote_2020), color = "black") + geom_bar(alpha = 0.7, color = "black") + 
   scale_fill_manual(values = c("#FF4040", "#0000FF")) + labs(title = " Vote Distribution by Employment Status" , subtitle =  " A look into how eligible voters intend to vote based on employment status.", x = "Employment Status", y= "Number of votes", fill = "Nominee") +   theme(axis.text.x = element_text(angle = 35, hjust = 1, size = 5))
-
-
-
-# Looking at political views depending on political interest
-survey_data_reduced %>% drop_na(ideo5, interest) %>% ggplot(aes(x= ideo5, y=interest)) + 
-  geom_count(aes(size=..prop..), colour="lightgrey") +
-  geom_count(aes(size=..prop.., group=ideo5), colour="cornflowerblue")  +
-  scale_size(range = c(0,10), breaks=seq(0,1,by=0.2)) +
-  coord_fixed() +
-  theme_minimal()
-
-
-# matching state names to that of the post-stratification data
-survey_data_reduced <- survey_data_reduced %>% mutate(state = case_when(state == "AL" ~ "alabama", state == "AK" ~ "alaska", state == "AZ" ~ "arizona", state == "AR" ~ "arkansas", state == "CA" ~ "california",
-                                state == "CO" ~ "colorado", state == "CT" ~ "connecticut", state == "DE" ~ "delaware", state == "FL"~ "florida", state == "GA" ~ "georgia",
-                                state == "HI" ~ "hawaii", state == "ID" ~ "idaho", state == "IL" ~ "illinois", state == "IN" ~ "indiana", state == "IA" ~ "iowa",
-                                state == "KS" ~ "kansas", state == "KY" ~ "kentucky", state == "LA" ~ "louisiana", state == "ME"~ "maine", state == "MD"~"maryland", 
-                                state == "MA" ~ "massachusetts", state == "MI" ~ "michigan", state == "MN" ~ "minnesota", state == "MS" ~ "mississippi", state == "MO" ~ "missouri",
-                                state == "MT" ~ "montana", state == "NE" ~ "nebraska", state == "NV" ~ "nevada", state == "NH"~ "new hampshire", state == "NJ"~"new jersey",
-                                state == "NM" ~ "new mexico", state == "NY" ~ "new york", state == "NC" ~ "north carolina", state == "ND" ~ "north dakota", state == "OH" ~ "ohio",
-                                state == "OK" ~ "oklahoma", state == "OR" ~ "oregon", state == "PA" ~ "pennsylvania", state == "RI"~ "rhode island", state == "SC"~"south carolina",
-                                state == "SD" ~ "south dakota", state == "TN" ~ "tennessee", state == "TX" ~ "texas", state == "UT" ~ "utah", state == "VT" ~ "vermont",
-                                state == "VA" ~ "virginia", state == "WA" ~ "washington", state == "WV" ~ "west virginia", state == "WI"~ "wisconsin", state == "WY"~"wyoming"))
-                            
                                                            
 # Get proportion of those who voted for Trump by state in 2016
 avg_repub_votes <-survey_data_reduced %>% select(state, vote_2016) %>% filter(vote_2016 == "Donald Trump") %>% group_by(state, vote_2016) %>% count() %>% ungroup() %>% mutate(pct_trump = n/1948 * 100) %>% drop_na(state)
-avg_repub_votes$region <- tolower(avg_repub_votes$state)
+avg_repub_votes$region <- tolower(avg_repub_votes$sttate)
 trump_votes <- left_join(us, avg_repub_votes)
+
 # Visual of republican support by State
 trump_votes %>% ggplot(aes(x = long, y = lat, group = group, fill = pct_trump)) + coord_map(projection = "albers" ,lat0 = 39, lat1 = 45) + geom_polygon(color = "grey", size = 0.1) + labs(title = "Trump Votes by State", subtitle = "Distribution of Republican vote in 2016 election by state.") + 
   theme_map() + labs(fill = "Percentage") + scale_fill_gradient(low = "white", high = "red")
@@ -111,8 +110,8 @@ avg_dem_votes <-survey_data_reduced %>% select(state, vote_2016) %>% filter(vote
 
 avg_dem_votes$region <- tolower(avg_dem_votes$state)
 dem_votes <- left_join(us, avg_dem_votes)
-# Visual of democratic support by State
 
+# Visual of democratic support by State
 dem_votes %>% ggplot(aes(x = long, y = lat, group = group, fill = pct_clinton)) + coord_map(projection = "albers" ,lat0 = 39, lat1 = 45) + geom_polygon(color = "black", size = 0.1) + labs(title = "Clinton Votes by State", subtitle = "Distribution of Democratic vote in 2016 election by state.") + 
   theme_map() + labs(fill = "Percentage") + scale_fill_gradient(low = "gray", high = "navy blue")
 
--- scripts/03-modelling.R
+++ scripts/03-modelling.R
@@ -14,9 +14,7 @@ library(broom) # Helps make the regression results tidier
 library(here) # Helps make file referencing easier.
 library(tidyverse) # Helps make programming with R easier
 library(haven)
-
 library(stringr)
-
 # Look at some summary statistics to make sure the data seem reasonable
 summary(survey_data)
 
@@ -26,17 +24,31 @@ survey_data_reduced <-
   survey_data_reduced %>%
   mutate(vote_2020 = ifelse(vote_2020 == "Donald Trump", 1, 0))
 
+
 survey_data_reduced %>% 
-  summarise(raw_prop = sum(vote_2020) / nrow(survey_data_reduced))  
+  summarise(raw_prop = sum(vote_2020) / nrow(survey_data_reduced))  # no class bias for our response variable - results are fairy equal
 
-model <- glm(vote_2020 ~ gender + age + employment + race_ethnicity + household_income + state, family=binomial,
+# building model
+model <- glm(vote_2020 ~ sex + age + race + household_income + hispan + state, family=binomial(link = "logit"),
             data = survey_data_reduced)
-      
+summary(model)
 
+# Looking for indications of collinearity using correlation matrix of estimated coefficients
+car::vif(model) # no collinearity 
+# Influence diagnostic to check for any observations that might have an influence on the fitted model
 broom::tidy(model)
 ACS_data <- reduced_data
-
 ACS_data$estimate <-
   model %>%
   predict(newdata = ACS_data)
 
+
+### Testing Model
+set.seed(10)
+train <- survey_data_reduced %>% sample_frac(.70)
+predict <- predict(model,train, type = 'response')
+table_mat <- table(train$vote_2020, predict > 0.5)
+table_mat
+accuracy_Test <- sum(diag(table_mat)) / sum(table_mat)
+accuracy_Test
+# 63% - could be improved
